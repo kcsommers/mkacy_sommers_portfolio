bec82ba46e896f8fb6da0cbf35c0763e6630063e
Mark Sommers <kacysommers@gmail.com>
2018-09-07 15:43:58 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const bp = require('body-parser');
+const ejsLayouts = require('express-ejs-layouts');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/static'));
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bp.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.log(`Hooked on ${port}`);
+});
+
+
